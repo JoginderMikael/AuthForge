cf4d9df7f77325cde18789fa1fc4324b679c206f
Joginder Mikael <jogindermikael@gmail.com>
2026-03-10 00:36:35 +0300

    initial commit

---
+++ src/main/java/git/jogindermikael/MK_Auth_Service/MkAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package git.jogindermikael.MK_Auth_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MkAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/git/jogindermikael/MK_Auth_Service/MkAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package git.jogindermikael.MK_Auth_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
